ae553731939e1bd1d3f2c9d37d4a1589d958a527
Manuela Coronado Hernandez <78452776+manu-3130@users.noreply.github.com>
2023-11-08 20:53:18 -0500

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -18,3 +18,25 @@ function login() {
         showMessage('Credenciales incorrectas. Inténtelo de nuevo.');
     }
 }
+
+function cambiarContraseña(){
+    const password = document.getElementById('password').value;
+    const nuevaPassword = document.getElementById('nuevaPassword').value;
+    const nuevaPassword2 = document.getElementById('nuevaPassword2').value;
+    const user = users.find(u => u.password == password);
+
+    if(user){
+        if(nuevaPassword.length >= 8 && nuevaPassword == nuevaPassword2){
+            users[0].password = nuevaPassword
+            window.location.href = 'index.html';
+        }
+        else if(nuevaPassword.length < 8){
+            showMessage('Su contraseña debe de tener mas de 8 caracteres. Intentélo de nuevo.');
+        }else if (nuevaPassword != nuevaPassword2){
+            showMessage('Las contraseñas no son iguales. Inténtelo de nuevo.');
+        }
+
+    }else if (password != users[0].password){
+        showMessage('Credenciales incorrectas. Inténtelo de nuevo.');
+    }
+}
