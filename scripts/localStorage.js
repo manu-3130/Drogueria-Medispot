bbe9db00ef2b0b948acf1087db1dd68df685dfe7
Manuela Coronado Hernandez <78452776+manu-3130@users.noreply.github.com>
2023-11-22 21:35:58 -0500

    Delete scripts/localStorage.js

--- scripts/localStorage.js
+++
@@ -1,6 +0,0 @@
-const contraseña = document.getElementById("nuevaPassword").value;
-
-localStorage.setItem("nombre", "usuario");
-localStorage.setItem("contraseña", contraseña);
-let nombreGuardado = localStorage.getItem("nombre");
-console.log("Valor guardado: ", nombreGuardado);
